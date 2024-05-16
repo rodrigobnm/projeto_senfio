a1ed15fea9028fbec810480f25976018d9d02aa6
rodrigobnm <150079017+rodrigobnm@users.noreply.github.com>
2024-05-15 21:07:53 -0300

    .

--- senfio/api/views.py
+++ senfio/api/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def entrou(request):
+    return render(request, 'entrou.html')
+
+def login(request):
+    if request.method == 'POST':
+        data = request.POST 
+        username = data.get('username')
+        password = data.get('password')
+
+        if username == 'admin' and password == 'admin':
+            return render(request, 'index.html', {'success': True})
+        else:
+            return render(request, 'index.html', {'success': False, 'message': 'Credenciais inválidas'})
